f5aa218d82c8859edc113f0b8e61346a6db5b687
Joe Spinelli <joe.spinelli@hidglobal.com>
2023-11-17 16:28:12 -0600

    chapter 03: Class Methods

--- play.php
+++ play.php
@@ -5,6 +5,33 @@ class Ship {
 
   // Creates the property name that can be used by objects instantiated from this class
   public $name;
+
+//  Here assigning a default value for the following 3 properties in case one isn't set when instantiated in an object
+  public $weaponPower = 0;
+
+  public $jediFactor = 0;
+
+  public $strength = 0;
+
+//  Method is just like a function except it lives inside of the class and therefore have to call it on the object
+  public function sayHello() {
+    echo "Herro!";
+  }
+  public function weaponPower() {
+    return $this->name;
+  }
+
+  public function jediFactor() {
+    return $this->name;
+  }
+
+  public function strength() {
+    return $this->name;
+  }
+
+  public function getName() {
+    return $this->name;
+  }
 }
 
 // Instantiating an object from the class Ship using the keyword "new", an object being a filled in
@@ -15,8 +42,19 @@ class Ship {
 $myShip = new Ship();
 // use (arrow) -> syntax to grab properties from inside object
 $myShip->name = 'Batwing';
+$myShip->weaponPower = 69;
 
 echo 'Ship name: ' . $myShip->name;
 
-// Summary:
+// Chapter 02 Summary:
 // Real diff. between Object and Array is that Object has a class that defines all possible properties that it can hold
+
+echo '<hr/>';
+// Use () to signify you're calling a method and not a property
+$myShip->sayHello();
+
+echo '<hr/>';
+echo $myShip->getName();
+
+echo '<hr/>';
+var_dump($myShip->weaponPower);
